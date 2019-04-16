9f75ce2415d46ac13c66c470208b15bc832372c6
yunzi7758 <>
2019-04-16 22:43:09 +0800

    f

---
+++ hsp-khsys-center-provider/src/main/java/com/yunzi/khsys/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.deepexi.trade.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @version V1.0
+ * @author: LuoGuang
+ * @Package com.deepexi.member.config
+ * @Description:
+ * @date: 2019/3/4 16:01
+ */
+@Configuration
+public class CorsConfig {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
